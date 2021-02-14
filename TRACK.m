a8dde08756559b9d1f08779a459c952462eb3f12
wliu01 <57687551+heccinchomker@users.noreply.github.com>
2021-02-14 14:23:37 -0500

    Some documentation

--- TRACK.m
+++ TRACK.m
@@ -2,10 +2,12 @@ classdef TRACK
     %TRACK Summary of this class goes here
     %   Detailed explanation goes here
     
+    % Track parameters
+    % Tracklength and trackradius: parallel lists.
     properties
-        name
-        trackradius
-        tracklength
+        name % Name of the track
+        trackradius % array of radiuses
+        tracklength % array of lengths
     end
     
     methods
